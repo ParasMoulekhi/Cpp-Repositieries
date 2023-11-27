f892ceaf581f5b6c891350939179dc6ab28bd6b4
Paras <parasmoulekhi11@gmail.com>
2023-11-27 22:46:20 +0530

    initial commit

---
+++ cpp practice again/gcd.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+  int a,b,gcd;
+  cout<<"Enter two numbers: ";
+  cin>>a>>b;
+
+  if(a>b){
+    int temp=a;
+    a=b;
+    b=temp;
+  }
+
+  for(int i=1;i<=b;i++){
+    if(a%i==0 && b%i==0){
+      gcd=i;
+    }
+  }
+  cout<<"GCD = "<<gcd;
+  return 0;
+}
